77be6c33cc31a1cb756b180b2ee7f076485bf079
Neeraj Girolkar <neeraj.girolkar@domo.com>
2016-04-05 16:21:08 +0530

    Spring HelloWorld using FileSystemXmlApplicationContext

--- src/main/java/com/spring/main/Main.java
+++ src/main/java/com/spring/main/Main.java
@@ -1,13 +1,13 @@
 package com.spring.main;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import com.spring.examples.HelloSpring;
 
 public class Main {
     public static void main(String[] args) {
-	    ApplicationContext context = new ClassPathXmlApplicationContext("spring-beans.xml");
+	    ApplicationContext context = new FileSystemXmlApplicationContext("//Volumes//Disk_Drive//Windows_Data//Projects//Spring//Spring-Hello-World-XML//src//main//resources//spring-beans.xml");
 	    HelloSpring helloSpringBean = context.getBean("helloSpring", HelloSpring.class);
 	    helloSpringBean.greet();
     }
